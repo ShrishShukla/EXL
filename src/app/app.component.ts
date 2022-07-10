1135ef7dd084b323733c8de1b72f117a8b75ab68
IAMNISHANTSHUKLA <NISHANTSHUKLA2004@GMAIL.com>
2022-07-10 12:51:40 +0530

    EXL hackathon FE files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scheduler-x';
+  start_end_mark:Array<any> = [];
+  latlng:Array<any> = [
+    
+    [
+      23.0285312,
+      72.5262336
+    ],
+    [
+      19.0760,
+      72.8777
+    ],
+    [
+      50.2048,
+      55.2708
+    ]
+    ,
+    [
+		28.6139, 77.2090
+    ]
+  ];
+ 
+	public addresses: any[] = [{
+	  id: 1,
+	  address: '',
+	  street: '',
+	  city: '',
+	  country: ''
+	}];
+	constructor() {
+		this.start_end_mark.push(this.latlng[0]);
+		this.start_end_mark.push(this.latlng[this.latlng.length - 1]);
+	}
+  
+	ngOnInit() {
+  
+	}
+  
+	addAddress() {
+	  this.addresses.push({
+		id: this.addresses.length + 1,
+		address: '',
+		street: '',
+		city: '',
+		country: ''
+	  });
+	}
+  
+	removeAddress(i: number) {
+	  this.addresses.splice(i, 1);
+	}
+  
+	logValue() {
+	  console.log(this.addresses);
+	}
+  
+  
+  zoom: number = 1;
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'; 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    NgbModule,
+    AgmCoreModule.forRoot({
+      // please get your own API key here:
+      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+      apiKey: '?'
+    })
+    ,
+    ReactiveFormsModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
